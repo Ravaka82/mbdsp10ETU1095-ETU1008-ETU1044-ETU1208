977fad12aa676c0015aa1ba7ee486e41166a2ea6
Ravaka82 <89504880+Ravaka82@users.noreply.github.com>
2024-07-02 19:55:05 +0300

    Init Jakarta EE (backend)

---
+++ Backend_JakartaEE_API_REST/src/main/java/org/apache/maven/backend_jakartaee_api_rest/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package org.apache.maven.backend_jakartaee_api_rest.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
