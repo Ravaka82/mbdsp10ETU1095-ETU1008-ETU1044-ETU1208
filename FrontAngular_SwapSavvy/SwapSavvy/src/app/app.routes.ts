60eb9f40557f8558a8c63e2ff50dc827cf033b3c
Sarobidy <ratsaraefadahysarobidy@gmail.com>
2024-07-23 16:57:21 +0300

    Details de l'objet

--- FrontAngular_SwapSavvy/SwapSavvy/src/app/app.routes.ts
+++ FrontAngular_SwapSavvy/SwapSavvy/src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { ObjetCreateComponent } from './objet-create/objet-create.component';
 import { ObjetListComponent } from './objet-list/objet-list.component';
 import { ObjetUpdateComponent } from './objet-update/objet-update.component';
-
+import { DetailsObjetComponent } from './details-objet/details-objet.component';
 
 export const routes: Routes = [
   { path: 'create', component: ObjetCreateComponent },
   { path: 'list', component: ObjetListComponent },
   { path: 'update/:id', component: ObjetUpdateComponent },
-  { path: '', redirectTo: '/list', pathMatch: 'full' }
-  
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: 'DetailObjet/:id', component: DetailsObjetComponent }
 ];
