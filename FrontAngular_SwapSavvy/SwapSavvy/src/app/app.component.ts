8b206ec5e47c965cc736b209a3b3c9dae2534ba9
Ravaka82 <89504880+Ravaka82@users.noreply.github.com>
2024-07-24 19:47:36 +0300

    modification design with sidenav and toolbar (angular)

--- FrontAngular_SwapSavvy/SwapSavvy/src/app/app.component.ts
+++ FrontAngular_SwapSavvy/SwapSavvy/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ToolbarComponent,SidebarComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
