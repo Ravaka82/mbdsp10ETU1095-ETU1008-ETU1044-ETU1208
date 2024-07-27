8e23dbec282fffb638fe5c1627ea080493029298
Sarobidy <ratsaraefadahysarobidy@gmail.com>
2024-07-27 15:13:26 +0300

    projet

--- FrontAngular_SwapSavvy/SwapSavvy/src/app/app.routes.ts
+++ FrontAngular_SwapSavvy/SwapSavvy/src/app/app.routes.ts
@@ -5,12 +5,16 @@ import { ObjetUpdateComponent } from './objet-update/objet-update.component';
 import { DetailsObjetComponent } from './details-objet/details-objet.component';
 import { RechercheAvanceObjetComponent } from './recherche-avance-objet/recherche-avance-objet.component';
 import { ListRechercheSimpleComponent } from './list-recherche-simple/list-recherche-simple.component';
-
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 export const routes: Routes = [
+  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
   { path: 'create', component: ObjetCreateComponent },
   { path: 'list', component: ObjetListComponent },
   { path: 'update/:id', component: ObjetUpdateComponent },
-  { path: '', redirectTo: '/list', pathMatch: 'full' },
+
   { path: 'DetailObjet/:id', component: DetailsObjetComponent },
   { path: 'rechercheAvance', component: RechercheAvanceObjetComponent },
   { path: 'listeRechercheSimple', component: ListRechercheSimpleComponent }
