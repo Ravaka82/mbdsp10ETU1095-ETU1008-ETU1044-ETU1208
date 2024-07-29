28a6e1e60997b22ce94f71dfa9a9415550022106
Dina243 <81018819+Omega243@users.noreply.github.com>
2024-07-29 08:30:03 +0300

    Adding the removing files by git (correction des problèmes)

--- FrontAngular_SwapSavvy/SwapSavvy/src/app/services/auth.guard.ts
+++ FrontAngular_SwapSavvy/SwapSavvy/src/app/services/auth.guard.ts
@@ -4,17 +4,13 @@ import { inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 export const authGuard: CanActivateFn = (route, state) => {
 
-  // injection par programme (au lieu de le faire dans
-  // le constructeur d'un composant)
   let authService = inject(AuthService);
   let router = inject(Router);
   let snackBar = inject(MatSnackBar);
-  // si ça renvoie true, alors, on peut activer la route
   if(authService.isLogin()==false){
     snackBar.open("Vous devez se connecter  ", 'Fermer', {
-      duration: 3000, // Durée d'affichage du snackbar en millisecondes
+      duration: 3000,
     });
-    // et on retourne vers la page d'accueil
     router.navigate(["/signin"]);
     return false;
 
@@ -23,12 +19,10 @@ export const authGuard: CanActivateFn = (route, state) => {
 };
 export const adminGuard: CanActivateFn = (route, state) => {
 
-  // injection par programme (au lieu de le faire dans
-  // le constructeur d'un composant)
   let authService = inject(AuthService);
   let router = inject(Router);
   let snackBar = inject(MatSnackBar);
-  // si ça renvoie true, alors, on peut activer la route
+
   return authService.isAdmin()
   .then(authentifie => {
     if(authentifie) {
@@ -37,7 +31,7 @@ export const adminGuard: CanActivateFn = (route, state) => {
     } else {
       console.log("Vous n'êtes pas admin ! Navigation refusée !");
       snackBar.open("Vous devez se connecter en tant que admin pour pouvoir faire cette action ", 'Fermer', {
-        duration: 3000, // Durée d'affichage du snackbar en millisecondes
+        duration: 3000,
       });
       return false;
     }
