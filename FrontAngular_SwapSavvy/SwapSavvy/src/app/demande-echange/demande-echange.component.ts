9eecefc625b69eb442ad60130d4f68a6370d5506
Dina243 <81018819+Omega243@users.noreply.github.com>
2024-08-04 19:55:44 +0300

    Updating objet-list for section échange+sélection+Using Icons/Adding component demande-echange

---
+++ FrontAngular_SwapSavvy/SwapSavvy/src/app/demande-echange/demande-echange.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demande-echange',
+  standalone: true,
+  imports: [],
+  templateUrl: './demande-echange.component.html',
+  styleUrl: './demande-echange.component.css'
+})
+export class DemandeEchangeComponent {
+
+}
--- FrontAngular_SwapSavvy/SwapSavvy/src/app/objet-list/objet-list.component.ts
+++ FrontAngular_SwapSavvy/SwapSavvy/src/app/objet-list/objet-list.component.ts
@@ -138,4 +138,8 @@ export class ObjetListComponent implements OnInit {
         }
       );
   }
+
+  onSelectObjet(){
+
+  }
 }
