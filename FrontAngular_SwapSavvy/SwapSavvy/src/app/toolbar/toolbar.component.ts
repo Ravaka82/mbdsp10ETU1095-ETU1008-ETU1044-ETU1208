461e6b5276f491c4028836720181cd5f57fa5773
Dina243 <81018819+Omega243@users.noreply.github.com>
2024-08-16 01:35:52 +0300

    New Updates toolbar/app.component

--- FrontAngular_SwapSavvy/SwapSavvy/src/app/toolbar/toolbar.component.ts
+++ FrontAngular_SwapSavvy/SwapSavvy/src/app/toolbar/toolbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
+id: any|string;
 
 }
