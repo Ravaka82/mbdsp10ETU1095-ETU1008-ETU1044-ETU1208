c4565b9426a3b55a40756493490972e93d0a327b
Dina243 <81018819+Omega243@users.noreply.github.com>
2024-08-19 23:51:58 +0300

    Création Modèle Utilisateur et Objet Kotlin

---
+++ App_Mobile/Swapsavvy/app/src/main/java/com/app/swapsavvy/data/Objet.kt
@@ -0,0 +1,17 @@
+package com.app.swapsavvy.data
+
+import java.util.Date
+
+data class Objet(
+    val _id: String?,
+    val utilisateur_id: String,
+    val categorie_id: String,
+    val titre: String,
+    val description: String,
+    val date_creation: Date,
+    val date_modification: Date,
+    val statut: String,
+    val etat: String,
+    val valeur_estimee: Number,
+    val image_url: String
+)
