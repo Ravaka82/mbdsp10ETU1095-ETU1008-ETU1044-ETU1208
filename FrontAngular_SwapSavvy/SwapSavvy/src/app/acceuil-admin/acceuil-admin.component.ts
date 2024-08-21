c1575ce987cd16863e8cef2cbbd43a9f6a277af7
Sarobidy <ratsaraefadahysarobidy@gmail.com>
2024-08-21 23:18:44 +0300

    liste et suppression des comptes par admin

--- FrontAngular_SwapSavvy/SwapSavvy/src/app/acceuil-admin/acceuil-admin.component.ts
+++ FrontAngular_SwapSavvy/SwapSavvy/src/app/acceuil-admin/acceuil-admin.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilisateurService } from '../services/utilisateur.service';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-acceuil-admin',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './acceuil-admin.component.html',
   styleUrl: './acceuil-admin.component.css'
 })
 export class AcceuilAdminComponent implements OnInit{
-  
+  utilisateurs: any[] = [];
+  displayedColumns: string[] = ['nom', 'prenom', 'email','actions'];
 
-  constructor() {}
+  constructor(private utilisateurService: UtilisateurService) {}
 
   ngOnInit(): void {
-    
+    this.utilisateurService.getUtilisateurs().subscribe(data => {
+      this.utilisateurs = data;
+    });
+  }
+  onDelete(utilisateur: any): void {
+    if (confirm(`Êtes-vous sûr de vouloir supprimer l'utilisateur ${utilisateur.nom}?`)) {
+     
+      this.utilisateurService.deleteUtilisateur(utilisateur._id).subscribe(() => {
+        
+        this.utilisateurs = this.utilisateurs.filter(u => u._id !== utilisateur._id);
+      });
+    }
   }
 }
