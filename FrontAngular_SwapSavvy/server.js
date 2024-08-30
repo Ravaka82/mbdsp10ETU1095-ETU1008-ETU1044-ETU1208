4b002f57a3049c89c1b1c19bec0452e00dc04322
Sarobidy <ratsaraefadahysarobidy@gmail.com>
2024-08-30 16:13:15 +0300

    Update server.js

--- FrontAngular_SwapSavvy/server.js
+++ FrontAngular_SwapSavvy/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "FrontAngular_SwapSavvy/SwapSavvy/dist/swap-savvy/browser"));
+app.use(express.static(path.join(__dirname, "FrontAngular_SwapSavvy/SwapSavvy/dist/swap-savvy/browser")));
 
 app.get("/*", function (req, res) {
  res.sendFile(path.join(__dirname + "FrontAngular_SwapSavvy/SwapSavvy/dist/swap-savvy/browser/index.html"));
